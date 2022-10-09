45351f3b1e00cb25dd1f856a80f49812df438b8e
Fedor-Furik <107797365+Fedor-Furik@users.noreply.github.com>
2022-10-09 15:58:54 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -8,7 +8,7 @@ if (n <= 100 && n > 0) {
   console.log('false')
 }
 
-if (n > 0 || n <= 100) {
+if (n >= 0 || n <= 100) {
    console.log('true')
 } else {
    console.log('false')
@@ -22,16 +22,11 @@ Den: 1000,
 Matt: 5000,
 Steve: 2000
 }
-const namesofeng = {
-name1: 'Den',
-name2: 'Matt',
-name3: 'Steve'
+for (const key in engineers) {
+    console.log(`Заработная плата ${key} составляет ${engineers[key]} рублей.`)
 }
-console.log(`Заработная плата ${namesofeng.name1} составляет ${engineers.Den} рублей.`)
-console.log(`Заработная плата ${namesofeng.name2} составляет ${engineers.Matt} рублей.`)
-console.log(`Заработная плата ${namesofeng.name3} составляет ${engineers.Steve} рублей.`)
 
- 3 Создать массив из 5 элементов. Используя цикл for, вывести каждый второй элемент массива в консоль.
+// 3 Создать массив из 5 элементов. Используя цикл for, вывести каждый второй элемент массива в консоль.
 
 massiv = [1, 2, 3, 4, 5]
 for (let i; i < 6 ; i += 2) {
@@ -41,10 +36,9 @@ for (let i; i < 6 ; i += 2) {
 // 4 Есть массив произвольных чисел: let numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
 // Вывести в консоль значения всех элементов массива и соответствующие им индексы в таком виде: Индексу 0 соответствует число 42 Индексу 1 соответствует число 65 и т.д.
 
-let numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
-for(let i = 0; i < numbers.lenght; i++) {
-console.log(`Index: ${i}`)   
-console.log(numbers[i])
+let number = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
+for(let i = 0; i < number.lenght; i++) {
+console.log(`Индексу ${i} соответствует число ${number[i]} `)   
 }
 
 // 5 Дан массив объектов, например: Добавить в каждый объект дополнительное поле usersAnswer со значением null.Решение должно работать для массива любой длины.
@@ -58,6 +52,11 @@ let questions = [{
     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
     corAnswer: 0
 }];
+
+for (let i = 0;i < questions.length; i++) {
+    questions[i].usersAnswer = 'null';
+}
+console.log(questions)
 
 //6 Есть массив произвольных чисел: 1) Вывести в консоль все элементы массива, используя 2 разных цикла: for-of и for со счетчиком 
 //2) Посчитать и вывести в .консоль сумму элементов в массиве. Подсказка: в задачах такого типа нужно заранее предусмотреть переменную (например, sum) и записать в нее начальное значение (в случае с суммой - ноль). 
@@ -110,13 +109,16 @@ console.log(max);
 
 //5)
 
+max = numbers[0]
+let index = 0;
 for (let i = 0; i < numbers.length; i++) {
     if(numbers[i] > max){
         max = numbers[i];
+        index = i
        }
 
 }
-console.log(numbers.indexOf(max));
+console.log(`${max} с индексом ${index}`);
 
  //7 Определить массив let arr = [5, 4, 3, -3, -10, -1, 8, -20, 0]
 //dСоздать новый массив из элементов массива arr, но в новом должны содержаться только положительные элементы.
@@ -156,14 +158,19 @@ for (let i = 0; i < users.length; i++) {
 }
 
 // 10 Задать массив слов. Например:
+//let vegetables = ["морковь", "баклажан", "репа", "топинамбур"];
 //1) В цикле сформировать массив объектов с ключами word (само слово), length (длина слова):
 //[{word:'морковь', length: 7}, {word:'баклажан', length: 8} и т.п.]
 //Вывести этот массив в консоль.
 //Подсказка: длину строки можно определить с помощью метода .length. Например, для переменной let string = 'text' это будет string.length (результат - 4, т.к. слово text состоит из 4 символов)
 //2) Пройтись по полученному массиву объектов и вывести в консоль строки вида "слово - длина_слова", например "картошка - 8"
-//let vegetables = ["морковь", "баклажан", "репа", "топинамбур"];
 
-let vegetables = ["морковь","баклажан","репа","топинамбур"];
+let vegetables = ["морковь", "баклажан", "репа", "топинамбур"];
+let vegapro = [{},{},{},{}];
 for (let i = 0; i < vegetables.length; i++) {
-      console.log(`word: ${vegetables[i]}, lenght: ${vegetables[i].length}`)
+    vegapro[i].word = vegetables[i];
+    vegapro[i].lenght = vegetables[i].length;
+}
+for (let i = 0; i < vegapro.length; i++) {
+    console.log(`${vegapro[i].word} - ${vegapro[i].lenght}`)
 }
